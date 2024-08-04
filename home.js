58a23b2e8f30d78506c57b85d1be76cd7157b1ee
Rishaan Kotian <75593826+rishaank@users.noreply.github.com>
2024-08-03 23:21:32 -0700

    ui tweaks + stuff

---
+++ home.js
@@ -0,0 +1,27 @@
+document.getElementById("R").addEventListener("mouseover", function () {
+  document.getElementById("K").style.opacity = 1;
+});
+
+document.getElementById("I").addEventListener("mouseover", function () {
+  document.getElementById("O").style.opacity = 1;
+});
+
+document.getElementById("S").addEventListener("mouseover", function () {
+  document.getElementById("T").style.opacity = 1;
+});
+
+document.getElementById("H").addEventListener("mouseover", function () {
+  document.getElementById("I2").style.opacity = 1;
+});
+
+document.getElementById("A").addEventListener("mouseover", function () {
+  document.getElementById("A3").style.opacity = 1;
+});
+
+document.getElementById("A2").addEventListener("mouseover", function () {
+  document.getElementById("N2").style.opacity = 1;
+});
+
+document.getElementById("N").addEventListener("mouseover", function () {
+  document.getElementById(".").style.opacity = 1;
+});
