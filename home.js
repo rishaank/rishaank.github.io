9d9059ea34bd61b2d9008e123b6bf4e3192a8cf9
Rishaan Kotian <rishaan.kotian@gmail.com>
2026-01-11 15:23:40 -0800

    Improve responsive design and update content
    Enhanced mobile responsiveness across about, home, and projects pages by refining CSS media queries and layout styles. Updated about page content and icon sources, improved project descriptions and details, and added new animation effects to the home page. Various minor UI/UX improvements and code cleanups were also made.

--- home.js
+++ home.js
@@ -1,9 +1,23 @@
-document.querySelectorAll(".mainTitle").forEach((element, index) => {
-  element.addEventListener("mouseover", function () {
-    const hiddenElement = document.querySelectorAll(".hidden")[index];
-    if (hiddenElement) {
-      hiddenElement.style.opacity = 1;
-      document.getElementById("hint").style.opacity = 0;
-    }
+const indexList = [];
+window.setTimeout(() => {
+  document.querySelectorAll(".mainTitle").forEach((element, index) => {
+    element.addEventListener("mouseover", function () {
+      const hiddenElement = document.querySelectorAll(".hidden")[index];
+      const hiddenBox = document.querySelector(".hiddenBox");
+      if (hiddenElement) {
+        hiddenBox.style.opacity = 1;
+        hiddenElement.style.opacity = 1;
+        document.getElementById("hint").style.opacity = 0;
+        if (!indexList.includes(index)) {
+          indexList.push(index);
+        }
+      }
+      if (indexList.length == 7) {
+        window.setTimeout(() => {
+          hiddenBox.classList.add("boxReveal");
+          document.querySelector(".shiftDown").style.paddingTop = "2.5%";
+        }, 200);
+      }
+    });
   });
-});
+}, 200);
